85d77c35bdbf67341ad362f77373960ca679d547
saisrujana <saisrujana.nalam@jobiak.com>
2022-05-17 10:15:17 +0530

    project acchitect initiating the first push of project src

---
+++ stringbootvalidation43/src/main/java/com/jobiak/mvxjsp/Stringbootvalidation43Application.java
@@ -0,0 +1,13 @@
+package com.jobiak.mvxjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stringbootvalidation43Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stringbootvalidation43Application.class, args);
+	}
+
+}
---
+++ stringbootvalidation43/src/test/java/com/jobiak/mvxjsp/Stringbootvalidation43ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobiak.mvxjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stringbootvalidation43ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
